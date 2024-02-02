5e1a2e31c3e5586e88b5517fcf93e518b24c83fb
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 01:12:32 -0800

    project copmlete

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 import inquirer from "inquirer";
-let loop = true;
-while (loop) {
-    const answer = await inquirer.prompt([
-        {
-            type: "input",
-            name: "userSentence",
-            message: "Kindly enter your sentence to count the words",
-        },
-    ]);
-    const words = answer.userSentence.trim().split(" ");
-    const totalwords = words.length;
-    console.log(`the total words of your sentances are: ${totalwords}`);
-    let answers = await inquirer.prompt([
-        {
-            type: "confirm",
-            name: "again",
-            message: "Do you wantto count the words of sentence",
-        },
-    ]);
-    loop = answers.again;
+import showBanner from "node-banner";
+import gradient from "gradient-string";
+(async () => {
+    await showBanner("Word Counter", "Word Counter is used count the words of your Given Sentences", "red", "blue");
+})();
+async function fun() {
+    let loop = true;
+    while (loop) {
+        const answer = await inquirer.prompt([
+            {
+                type: "input",
+                name: "userSentence",
+                message: gradient.rainbow("Kindly enter your sentence to count the words"),
+            },
+        ]);
+        const words = answer.userSentence.trim().split(" ");
+        const totalwords = words.length;
+        console.log(gradient.rainbow(`the total words of your sentances are: ${totalwords}`));
+        let answers = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "again",
+                message: gradient.rainbow("Do you wantto count the words of sentence"),
+            },
+        ]);
+        loop = answers.again;
+    }
+    console.log(gradient.rainbow("Thanks for using our Word Conter App"));
 }
+setTimeout(() => {
+    fun();
+}, 1000);
--- app.ts
+++ app.ts
@@ -1,25 +1,48 @@
 import inquirer from "inquirer";
+import showBanner from "node-banner";
+import gradient from "gradient-string";
+
+(async () => {
+  await showBanner(
+    "Word Counter",
+    "Word Counter is used count the words of your Given Sentences",
+    "red",
+    "blue"
+  );
+})();
+
 type Answer = {
   userSentence: string;
 };
-let loop = true;
-while (loop) {
-  const answer: Answer = await inquirer.prompt([
-    {
-      type: "input",
-      name: "userSentence",
-      message: "Kindly enter your sentence to count the words",
-    },
-  ]);
-  const words = answer.userSentence.trim().split(" ");
-  const totalwords = words.length;
-  console.log(`the total words of your sentances are: ${totalwords}`);
-  let answers = await inquirer.prompt([
-    {
-      type: "confirm",
-      name: "again",
-      message: "Do you wantto count the words of sentence",
-    },
-  ]);
-  loop = answers.again;
+async function fun() {
+  let loop = true;
+  while (loop) {
+    const answer: Answer = await inquirer.prompt([
+      {
+        type: "input",
+        name: "userSentence",
+        message: gradient.rainbow(
+          "Kindly enter your sentence to count the words"
+        ),
+      },
+    ]);
+    const words = answer.userSentence.trim().split(" ");
+    const totalwords = words.length;
+    console.log(
+      gradient.rainbow(`the total words of your sentances are: ${totalwords}`)
+    );
+    let answers = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "again",
+        message: gradient.rainbow("Do you wantto count the words of sentence"),
+      },
+    ]);
+    loop = answers.again;
+  }
+  console.log(gradient.rainbow("Thanks for using our Word Conter App"));
 }
+
+setTimeout(() => {
+  fun();
+}, 1000);
